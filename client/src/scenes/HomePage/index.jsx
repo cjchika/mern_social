64441b3f1c2e45800e4aa807738f29e937bb745a
Chijioke Chika <8pluscreative@gmail.com>
2022-12-15 07:34:09 +0100

    Navbar

--- client/src/scenes/HomePage/index.jsx
+++ client/src/scenes/HomePage/index.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { Box } from "@mui/material";
+import NavBar from "../NavBar";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <Box>
+      <NavBar />
+    </Box>
+  );
 };
 
 export default HomePage;
