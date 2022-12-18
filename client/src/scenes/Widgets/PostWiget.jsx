a06a995b4dd25d0d3b3af4909beaae14a30a0a48
Chijioke Chika <8pluscreative@gmail.com>
2022-12-18 04:43:27 +0100

    Posts

---
+++ client/src/scenes/Widgets/PostWiget.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import {
+  ChatBubbleOutlineOutlined,
+  FavoriteBorderOutlines,
+  FavouriteOutlined,
+  ShareOutlined,
+} from "@mui/icons-material";
+import { Box, Divider, IconButton, Typography, useTheme } from "@mui/material";
+import FlexBetween from "../../Components/FlexBetween";
+import Friend from "../../Components/Friend";
+
+const PostWidget = () => {
+  return <div>PostWidget</div>;
+};
+
+export default PostWidget;
